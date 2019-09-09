3f5971c6846477a77fda73dd7b297df8706dfd7f
Mario Ernesto Basurto Medrano <mario@d80.mx>
2019-09-09 13:31:07 -0500

    Update cata-dip-gen.php

--- mod/dip/cata-dip-gen.php
+++ mod/dip/cata-dip-gen.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once("cnx/swgc-mysql.php");
 require_once("cls/cls-sistema.php");
 //include("../../inc/fun-ini.php");
@@ -8,9 +11,7 @@ echo $url; die();
 
 $html=file_get_contents($url);
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 
 $html=str_replace('font-size:14px;','font-size:12px;',$html);
