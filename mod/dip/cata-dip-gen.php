7f50dae5b43c2d621382d21c872b88efc4facbfd
Mario Ernesto Basurto Medrano <mario@d80.mx>
2019-09-11 12:03:40 -0500

    Update cata-dip-gen.php

--- mod/dip/cata-dip-gen.php
+++ mod/dip/cata-dip-gen.php
@@ -8,28 +8,48 @@ require_once("../../cls/cls-sistema.php");
 include("../../inc/fun-ini.php");
 $url = obtenerURL()."mod/dip/light-dip-gen.php?eCodRegistro=".$_GET['v1'];
 
-//echo $url; 
-//die();
+//$html=file_get_contents($url);
 
-$html=file_get_contents($url);
 
-$fichero = sprintf("%07d",$_GET['v1']).".html";
+//==============================================================
+$select = "SELECT bc.*,brc.eCodRegistro, su.tTitulo, su.tNombre, su.tApellidos FROM BitCursos bc INNER JOIN BitRegistrosCursos brc ON brc.eCodCurso=bc.eCodCurso INNER JOIN SisUsuarios su ON su.eCodUsuario=brc.eCodUsuario = ".$_GET['v1'];
+$rsRegistro = mysql_query($select);
+$rRegistro = mysql_fetch_array($rsRegistro);
 
-unlink ("./".fichero);
+$html = '
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    
+    <title>Detalle del Evento</title>
+    <script src="//use.edgefonts.net/dynalight.js"></script>
+    <style>
+    
+    .h1
+        {
+            font-family: dynalight, sans-serif;
+        }
+    </style>
+</head>
 
-$pf = fopen($fichero,"w");
-fwrite($pf,$html);
-fclose($pf);
-//echo '<textarea>'.file_get_contents($url).'</textarea>';
+<body style="margin:0; padding:0; background-image:url(\''.obtenerURL().'cla/'.$rRegistro{'tArchivoDiploma'}.'\'); background-repeat: no-repeat; background-size: 100% 100%;">
+   
+    <table width="100%" height="100%">
+    <tr>
+        <td height="464" colspan="2"></td>
+    </tr>
+    <tr>
+        <td height="198" valign="middle" align="center" colspan="2"><h1 style="font-family: dynalight, sans-serif;">'.utf8_encode($rRegistro{'tTitulo'}.' '.$rRegistro{'tNombre'}.' '.$rRegistro{'tApellidos'}).'</h1></td>
+    </tr>
+    <tr>
+        <td colspan="2" height="130"></td>
+    </tr>
+    </table>
+    
+</body>
+</html>
+';
 
-//$html=str_replace('font-size:14px;','font-size:12px;',$html);
-
-$url = obtenerURL()."mod/dip/".$fichero;
-$html=file_get_contents($url);
-//echo $html; 
-
-//die();
-
+//==============================================================
 
 
 //==============================================================
